f72900c6e1c1eea7aacca97e9a9103d929e06207
胡雨潇 <136304230+Xiao254182@users.noreply.github.com>
2024-09-19 13:35:55 +0800

    Update d4r_setup.sh

--- d4r_setup.sh
+++ d4r_setup.sh
@@ -3,7 +3,7 @@
 #部署go语言环境
 wget https://golang.google.cn/dl/go1.23.1.linux-amd64.tar.gz && tar -zxf go1.23.1.linux-amd64.tar.gz -C /usr/local
 echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile && echo "export GOPROXY=https://goproxy.io,direct" >> /etc/profile && echo "export GOPATH=/opt/d4r" >> /etc/profile
-&& source /etc/profile
+ && source /etc/profile
 #部署d4r
 mkdir -p /opt/d4r
 wget https://github.com/Xiao254182/D4R/raw/refs/heads/master/d4r.tar.gz && tar -zxf d4r.tar.gz -C /opt/d4r && cd /opt/d4r
