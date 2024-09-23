f32f42a253c355e775cbf428ddaab9e594135a6c
胡雨潇 <136304230+Xiao254182@users.noreply.github.com>
2024-09-23 20:05:54 +0800

    更新从docker-compose页面查看logs

--- ps/ps.go
+++ ps/ps.go
@@ -35,7 +35,7 @@ func getDockerOutput(command string, args ...string) ([][]string, error) {
 		if line = strings.TrimSpace(line); line != "" {
 			fields := re.Split(line, -1)
 
-			// 确保 fields 至少有 6 个元素 
+			// 确保 fields 至少有 6 个元素
 			if len(fields) < 7 {
 				// 如果字段数不足 6，补充空值
 				for len(fields) < 7 {
@@ -68,4 +68,39 @@ func parseContainerIDs(output []byte) []string {
 	}
 
 	return containerIDs
+}
+
+// GetDockerCompose 获取当前运行中的 Docker Compose 集群
+func GetDockerCompose() ([][]string, error) {
+	return getDockerComposeOutput("docker-compose", "ls")
+}
+
+// GetRunningContainerIDs 获取当前运行中的 Docker 容器的 ID
+func GetRunningDockerComposeName() ([]string, error) {
+	output, err := exec.Command("docker-compose", "ls", "-q").Output()
+	if err != nil {
+		return nil, err
+	}
+	return parseContainerIDs(output), nil
+}
+
+// getDockerComposeOutput 通用函数，执行给定的 Docker 命令并解析结果
+func getDockerComposeOutput(command string, args ...string) ([][]string, error) {
+	output, err := exec.Command(command, args...).Output()
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(output), "\n")
+	var dockerCompose [][]string
+	re := regexp.MustCompile(`\s{2,}`)
+
+	for _, line := range lines[1:] { // 跳过表头
+		if line = strings.TrimSpace(line); line != "" {
+			fields := re.Split(line, -1)
+			dockerCompose = append(dockerCompose, fields)
+		}
+	}
+
+	return dockerCompose, nil
 }
